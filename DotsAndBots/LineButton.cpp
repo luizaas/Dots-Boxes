becf28fc37f249b554e88bb7e2140092cc362cfd
Aluno <aluno@decom-OptiPlex-780>
2017-05-23 16:37:09 -0300

    23/05

--- DotsAndBots/LineButton.cpp
+++ DotsAndBots/LineButton.cpp
@@ -1,20 +1,55 @@
 #include "LineButton.h"
 
-LineButton::LineButton(QWidget *parent) : QPushButton(parent){
+LineButton::LineButton(QWidget *parent) :
+    QPushButton(parent),
+    m_mode(Qt::Horizontal),
+    m_color(QColor("#663399")) {
     this->updateStyleSheet();
 }
+
 LineButton::~LineButton(){}
 
+void LineButton::setMode(Qt::Orientation mode){
+    if(mode != m_mode){
+         m_mode = mode;
+         emit modeChanged(m_mode);
+    }
+}
+
+void LineButton::setColor(QColor color){
+      if(color != m_color){
+           m_color = color;
+           emit colorChanged(m_color);
+            this->setEnabled(false);
+      }
+}
+
 void LineButton::updateStyleSheet(){
-    this->setStyleSheet("\
+   QString rgba = m_color.name(QColor::HexArgb);
+
+    this->setStyleSheet(QString("\
         QPushButton { \
+            border-radius: 0px;            \
             border: 0px solid transparent; \
         }\
-        QPushButton { \
+        QPushButton[mode=\"1\"] { \
             border-top-width: 15px;\
         }\
-        QPushButton:hover{\
+        QPushButton[mode=\"2\"] { \
+            border-left-width: 15px;\
+        }\
+        QPushButton[mode=\"1\"]:!enabled { \
+            border-color: qlineargradient(x1:0, y1:0, x2:0, y2:1,        \
+                            stop:0 transparent, stop: 0.2 transparent, stop: 0.5 %1, \
+                            stop: 0.8 transparent, stop:1 transparent);              \
+        }          \
+        QPushButton[mode=\"2\"]:!enabled { \
+            border-color: qlineargradient(x1:0, y1:0, x2:1, y2:0,        \
+                            stop:0 transparent, stop: 0.2 transparent, stop: 0.5 %1, \
+                            stop: 0.8 transparent, stop:1 transparent);              \
+        }\
+        QPushButton:enabled:hover{\
             border-image: url(':/dotts');\
         }\
-    ");
+    ").arg(rgba));
 }
--- DotsAndBots/LineButton.h
+++ DotsAndBots/LineButton.h
@@ -5,13 +5,22 @@
 
 class LineButton : public QPushButton {
     Q_OBJECT
+    Q_PROPERTY(Qt::Orientation mode READ mode WRITE setMode NOTIFY modeChanged)
+    Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
 public:
     explicit LineButton(QWidget *parent = 0);
     virtual ~LineButton();
+    Qt::Orientation mode() const {return m_mode; } //metodo get
+    QColor color() const {return m_color;}
 signals:
-
+    void modeChanged(Qt::Orientation mode); //sinal avisa qual a nova orientaçao
+    void colorChanged(QColor color);
 public slots:
-
+    void setMode(Qt::Orientation mode); //metodo set
+    void setColor(QColor color);
+private:
+    Qt::Orientation m_mode;
+    QColor m_color;
 private slots:
     void updateStyleSheet();
 };
